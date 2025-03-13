79a6902a8b8016a3f70ec760df694cefd2650ad7
ravikalia-01 <kravi014@gmail.com>
2025-03-13 17:02:52 -0400

    added footer

--- fb.js
+++ fb.js
@@ -1,1 +1,13 @@
 //this page is created by Ravi Kumar
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let message = document.getElementById("message");
+    
+    if (username === "admin" && password === "password123") {
+        alert("Login successful!");
+        message.textContent = "";
+    } else {
+        message.textContent = "Invalid username or password";
+    }
+}
